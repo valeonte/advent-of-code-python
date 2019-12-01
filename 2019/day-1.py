e1659874d7c34feb2290f432e3a2f003fe4dbf49
Eftychios Valeontis <valeontis@gmail.com>
2019-12-01 08:00:27 +0000

    day 1

---
+++ 2019/day-1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  1 06:54:36 2019
+
+@author: Eftychios
+"""
+from math import floor
+
+def calc_fuel_required(mass: int, recursive: bool = False) -> int:
+    fuel = floor(mass/3) - 2
+    
+    if fuel <= 0:
+        fuel = 0
+    
+    if recursive:
+        if fuel == 0:
+            return fuel
+        else:
+            return fuel + calc_fuel_required(fuel, True)
+    else:
+        return fuel
+
+
+with open("2019/day-1-input.txt", "r") as f:
+    contents = f.read()
+
+all_numbers = [int(stritem)for stritem in contents.split("\n")]
+all_fuels = [calc_fuel_required(mass) for mass in all_numbers]
+
+answer_1 = sum(all_fuels)
+
+all_fuels_rec = [calc_fuel_required(mass, True) for mass in all_numbers]
+
+answer_2 = sum(all_fuels_rec)
+
+print(answer_1, answer_2)
