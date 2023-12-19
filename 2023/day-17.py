d324fe4732c75f4f5092bce75f7aea224c80c5d9
Eftychios Valeontis <valeontis@gmail.com>
2023-12-19 15:58:32 +0000

    horrendous day 17 done

--- 2023/day-17.py
+++ 2023/day-17.py
@@ -31,8 +31,8 @@ inp_string = r"""2413432311323
 2546548887735
 4322674655533"""
 
-# with open("inputs/day-17.txt", "r") as f:
-#     inp_string = f.read()
+with open("inputs/day-17.txt", "r") as f:
+    inp_string = f.read()
 
 
 inps = inp_string.split('\n')
@@ -57,30 +57,38 @@ class State:
 
     heading: Heading
     same_remaining: int = 3
+    min_same_left: int = 0
 
     def __lt__(self, other):
         return (self.i, self.j) < (other.i, other.j)
 
     def turn_left(self) -> 'State':
+        if self.min_same_left > 0:
+            return None
         new_heading = self.heading.value - 1
         if new_heading < 0:
             new_heading = Heading.W
         else:
             new_heading = Heading(new_heading)
+        return replace(self, heading=new_heading, same_remaining=10, min_same_left=4)
         return replace(self, heading=new_heading, same_remaining=3)
 
     def turn_right(self) -> 'State':
+        if self.min_same_left > 0:
+            return None
         new_heading = self.heading.value + 1
         if new_heading > 3:
             new_heading = Heading.N
         else:
             new_heading = Heading(new_heading)
+        return replace(self, heading=new_heading, same_remaining=10, min_same_left=4)
         return replace(self, heading=new_heading, same_remaining=3)
 
     def move_ahead(self) -> 'State':
         if self.same_remaining == 0:
             return None
-        changes = dict(same_remaining=self.same_remaining - 1)
+        changes = dict(same_remaining=self.same_remaining - 1,
+                       min_same_left=max(0, self.min_same_left - 1))
         if self.heading == Heading.N:
             if self.i == 0:
                 return None
@@ -103,16 +111,43 @@ class State:
 
 def get_potential_next(state: State) -> Iterator[State]:
     """Get all potential next nodes."""
+    left = state.turn_left()
+    right = state.turn_right()
+    states = [s.move_ahead() for s in [left, right, state] if s is not None]
     return sorted([s
-                   for s in [state.turn_left().move_ahead(),
-                             state.move_ahead(),
-                             state.turn_right().move_ahead()]
+                   for s in states
                    if s is not None], key=lambda x: 0 if x.heading == Heading.S or x.heading == Heading.E else 1)
 
 
 # Proper Dijkstra’s
 
-start = State(0, 0, heading=Heading.E)
+# start = State(0, 0, heading=Heading.E)
+
+# queue = []
+# heapq.heappush(queue, (0, start))
+
+# seen = set()
+# while queue:
+#     cost, state = heapq.heappop(queue)
+#     #print(cost, state)
+
+#     if state.i == max_i and state.j == max_j:
+#         break
+
+#     if state in seen:
+#         continue
+
+#     seen.add(state)
+
+#     for n in get_potential_next(state):
+#         new_cost = cost + heat_map[n.i, n.j]
+#         heapq.heappush(queue, (new_cost, n))
+
+
+# print('Answer 1:', cost)
+
+
+start = State(0, 0, heading=Heading.E, same_remaining=10, min_same_left=4)
 
 queue = []
 heapq.heappush(queue, (0, start))
@@ -120,19 +155,18 @@ heapq.heappush(queue, (0, start))
 seen = set()
 while queue:
     cost, state = heapq.heappop(queue)
-    #print(cost, state)
 
-    if state.i == max_i and state.j == max_j:
+    if state.i == max_i and state.j == max_j and state.min_same_left == 0:
         break
 
     if state in seen:
         continue
 
+    #print(cost, state)
     seen.add(state)
 
     for n in get_potential_next(state):
         new_cost = cost + heat_map[n.i, n.j]
         heapq.heappush(queue, (new_cost, n))
 
-
-print('Answer 1:', cost)
+print('Answer 2:', cost)
