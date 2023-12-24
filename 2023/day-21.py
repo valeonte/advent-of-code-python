6e2849bb6f07528ebf52b0b5a8876632dd9721ee
Eftychios Valeontis <valeontis@gmail.com>
2023-12-24 16:10:50 +0000

    Day 21 - very tough but very rewarding in the end :)

--- 2023/day-21.py
+++ 2023/day-21.py
@@ -11,6 +11,7 @@ import json
 import re
 import math
 
+import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -35,8 +36,8 @@ inp_string = """...........
 ..........."""
 
 
-# with open("inputs/day-21.txt", "r") as f:
-#     inp_string = f.read()
+with open("inputs/day-21.txt", "r") as f:
+    inp_string = f.read()
 
 inps = inp_string.split('\n')
 
@@ -116,7 +117,8 @@ def get_reachable_inf(farm: InfiniteFarm, from_nodes: Set[Tuple[int, int]]) -> S
 
 
 ifarm = InfiniteFarm(farm)
-steps = 100
+steps = 300
+print('Populating first', steps, 'steps', dt.datetime.now())
 cur_nodes = {start_pos}
 all_lens = []
 while steps > 0:
@@ -124,28 +126,80 @@ while steps > 0:
     cur_nodes = get_reachable_inf(ifarm, from_nodes=cur_nodes)
     all_lens.append(len(cur_nodes))
 
+print('Done, doing derivs and finding period', dt.datetime.now())
 
-all_len = [all_lens[i+1] - all_lens[i] for i in range(len(all_lens)-1)]
-
-ft = [all_len[i] - all_len[i - 11] for i in range(11, len(all_len))]
-a = [ft[i + 11] == ft[i] for i in range(len(ft) - 11)]
-
-plt.plot(ft)
-plt.plot(ft)
-plt.show()
-
-period = 11
-period_start = 36
+deriv1 = [all_lens[i] - all_lens[i-1] for i in range(1, len(all_lens))]
+deriv2 = [deriv1[i] - deriv1[i-1] for i in range(1, len(deriv1))]
 
 
-idx = 47
-ps = idx - period_start
-one_period_sum = all_len[period_start:period_start + period]
-all_lens_val = all_len[0] + sum(all_len[:period_start]) \
-    + sum(all_len[period_start:period_start + (ps % period)]) \
-        + all_len[period_start + (ps % period) - 1] * (ps // period)
+def find_period(deriv2: List[int]) -> Tuple[int, int]:
+    """Attempt to auto-find period and offset."""
+    # numbers in an entire period should have the exact same sum
+    period = 5
+    while period < len(deriv2) // 2:
+        sums = [sum(deriv2[i-period:i])
+               for i in range(period, len(deriv2), period)]
+        period_sum = sums[-1]
+        cont_eq = 0
+        while cont_eq < len(sums) and sums[-1 - cont_eq] == period_sum:
+            cont_eq += 1
+        if cont_eq > 1:
+            period_start = (len(sums) - cont_eq) * period
+            first_period = deriv2[period_start:period_start + period]
+            return period_start, period, period_sum, first_period
 
-assert(all_lens_val == all_lens[idx])
+        period += 1
+    
+    raise Exception('Failed to find any period!')
 
 
-print('Answer 2:', len(cur_nodes))
+period_start, period, period_sum, first_period = find_period(deriv2)
+period_incs = [deriv2[i+period] - deriv2[i] for i in range(period_start, period_start + period)]
+
+
+def find_deriv1_value(x: int) -> int:
+    ret = deriv1[0]
+    if x <= period_start:
+        return ret + sum(deriv2[:x])
+    ret += sum(deriv2[:period_start])
+
+    periods = (x - period_start) // period
+    ret += periods * period_sum
+    for rem in range((x - period_start) % period):
+        ret += first_period[rem] + period_incs[rem] * periods
+
+    return ret
+
+
+# Confirming it works correctly
+for x in range(len(deriv1)):
+    assert(deriv1[x] == find_deriv1_value(x))
+
+
+def find_all_lens_value(x: int) -> int:
+    return all_lens[0] + sum([find_deriv1_value(i) for i in range(x)])
+
+
+for x in range(len(all_lens)):
+    if x == 0:
+        last_no = all_lens[0]
+        continue
+    last_no += find_deriv1_value(x - 1)
+    assert(all_lens[x] == last_no)
+
+print('All good, knocking it on the head now!')
+
+start = dt.datetime.now()
+steps = 26501365
+for x in range(steps):
+    if x == 0:
+        last_no = all_lens[0]
+        continue
+    last_no += find_deriv1_value(x - 1)
+    if x % 1_000_000 == 0:
+        spent = dt.datetime.now() - start
+        est = steps * spent/x - spent
+        print('x =', x, 'no =', last_no, 'spent =', spent, 'est finish =', est)
+
+print('Answer 2:', last_no)
+print('Done?')
